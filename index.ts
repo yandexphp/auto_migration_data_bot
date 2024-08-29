32591859e41bb2744c7017e1b6a1e577f9475ef8
yandexphp <yandexphp01@gmail.com>
2024-08-29 17:44:59 +0500

    Last update the bot - migration files [auto] - migration comments [auto] - migration by dictionary values [auto]

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import puppeteer from 'puppeteer'
 
 import {getAuthPass, getAuthUsername, getUrlLoginPage} from './src/utils'
-import {SceneSixteen} from './src/controllers/scene16'
+import {SceneSeventeen} from './src/controllers/scene17'
 import {WebSocketClose, WebSocketConnection} from './src/websocket'
 
 WebSocketConnection()
@@ -48,7 +48,7 @@ try {
             if (url.endsWith('/login') && method === 'POST') {
                 if (status === 302) {
                     console.info('Authorization Successfully!')
-                    new SceneSixteen(page)
+                    new SceneSeventeen(page)
                 } else {
                     console.warn('Authorization failed, please check your login or password')
                     throw new Error('Failed to login')
@@ -63,7 +63,7 @@ try {
             await page.type('input[id="username"]', getAuthUsername())
             await page.type('input[id="password"]', getAuthPass())
         } catch {
-            return new SceneSixteen(page)
+            return new SceneSeventeen(page)
         }
 
         await Promise.all([
---
+++ src/controllers/scene17/readme.md
@@ -0,0 +1,8 @@
+# Зарема - 0000117: Миграция данных Блока Протокольные поручения/Модуль регистрация участников
+
+- (4247 задач) https://ekap.kazatomprom.kz/projects/reg_ps/issues
+
+```env
+PAGE_EKAP_URL_ISSUE_LIST=/user/modules/07f3c0fe-28c0-4a70-aabe-f2720cd4634b/sections/a9d2687d-e641-49c8-8586-182e1d416f2e/bp/c3efeb77-192d-4030-9590-79b2964b95d8?page=0
+PAGE_EKAP_URL_ISSUE_FORM_CREATE=/user/modules/07f3c0fe-28c0-4a70-aabe-f2720cd4634b/sections/a9d2687d-e641-49c8-8586-182e1d416f2e/bp/c3efeb77-192d-4030-9590-79b2964b95d8/new
+```
